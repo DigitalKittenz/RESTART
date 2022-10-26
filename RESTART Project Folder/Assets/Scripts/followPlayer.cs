f5285f02431ec3a98da1d8c87bd6bfce423823f7
DigitalKittenz <114578234+DigitalKittenz@users.noreply.github.com>
2022-10-26 07:26:39 +0100

    still working on camera

--- RESTART Project Folder/Assets/Scripts/followPlayer.cs
+++ RESTART Project Folder/Assets/Scripts/followPlayer.cs
@@ -7,9 +7,15 @@ public class followPlayer : MonoBehaviour
     public Transform targetObject;
     private Vector3 initialOffset;
     private Vector3 cameraPosition;
+    public Vector3 positionChange;
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 pos = transform.position;
+        pos.x = -15;
+        pos.x = -15;
+   
+        transform.position = pos;
         initialOffset = transform.position - targetObject.position;   
     }
 
@@ -18,5 +24,6 @@ public class followPlayer : MonoBehaviour
     {
         cameraPosition = targetObject.position + initialOffset;
         transform.position = cameraPosition;
+        transform.position += positionChange;
     }
 }
